d7e40f5f1ad3c305e6fd2f87cd1dedf01fef824c
Xpeednet <129681844+Xpeednet@users.noreply.github.com>
2023-05-20 16:20:32 -0300

    Api de clima con icono en todos los footers

---
+++ js/clima.js
@@ -0,0 +1,33 @@
+new Vue({
+    el: '#app',
+    data() {
+      return {
+        loading: true,
+        temperature: null,
+        description: null,
+        iconLink: null,
+        apiKey: 'aef6a177b652a823651cb4bffc06e0cf',
+        city: 'Mar del Plata'
+      };
+    },
+    mounted() {
+      this.fetchWeather();
+    },
+    methods: {
+      fetchWeather() {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&lang=sp&appid=${this.apiKey}`;
+
+        axios.get(apiUrl)
+          .then(response => {
+            this.loading = false;
+            this.temperature = Math.round(response.data.main.temp - 273.15);
+            this.description = response.data.weather[0].description;
+            this.iconLink = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
+          })
+          .catch(error => {
+            console.error(error);
+            this.loading = false;
+          });
+      }
+    }
+  });
